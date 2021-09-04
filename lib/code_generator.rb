05872d8d20f44b6c0412db720d8448bf8a6d6554
Chris Hewitt <agop5134@gmail.com>
2021-09-04 16:36:34 -0600

    create (working!)code generator.rb and code_generator_spec.rb

---
+++ lib/code_generator.rb
@@ -0,0 +1,18 @@
+class CodeGenerator
+  attr_reader :colors
+  def initialize
+    @colors = ['b','g','r','y']
+  end
+
+
+  def generate_secret
+   4.times.map do |color|
+     @colors.sample
+   end
+
+  end
+
+end
+
+ #iterating 4 times thru the string array, using sample method to pull different elements out
+ #every time, joining them together as string enclosed in ''
