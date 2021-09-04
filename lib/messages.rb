38a3868b76bfbdd3870add65d9ce097d4b159f94
Chris Hewitt <agop5134@gmail.com>
2021-09-04 11:54:39 -0600

    create insctuctions_message, quit_message, game_flow_message

--- lib/messages.rb
+++ lib/messages.rb
@@ -12,3 +12,21 @@ class Messages
     puts "That input is not valid.
     Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
   end
+
+  def instructions_message
+    puts "To play MASTERMIND, guess a
+    combination of these 4 colors:
+    (r)ed, (g)reen, (y)ellow, (b)lue.
+    enter (h)ome to return to welcome screen."
+  end
+
+  def quit_message
+    puts 'You have quit.'
+  end
+
+  def game_flow_message
+    puts "I have generated a secret color code consiting four colors:
+    (r)ed, (g)reen, (b)lue and (y)ellow. Can you guess it?
+    Enter your guess below!
+    >"
+  end
