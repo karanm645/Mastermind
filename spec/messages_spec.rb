7e594d548b5d5c68ae8456db8a726f7d9a43e252
Chris Hewitt <agop5134@gmail.com>
2021-09-04 11:59:13 -0600

    create message test for Messages class, welcome_message, quit_message

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -1,1 +1,20 @@
 require 'RSpec'
+require './lib/messages'
+
+RSpec.describe Messages do
+  it "exists" do
+    messages = Messages.new
+    expect(messages).to be_an_instance_of(Messages)
+  end
+
+  it "welcomes the player" do
+    messages = Messages.new
+    welcome_message = ''
+    expect(welcome_message).to eq('')
+  end
+
+  it "allows the player to quit"
+   messages = Messages.new
+   quit_message = ''
+   expect(quit_message) = ''
+  end
