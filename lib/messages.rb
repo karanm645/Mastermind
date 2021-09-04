747d266f154f5a70dcffdf05ee15a5aebb01edd6
Chris Hewitt <agop5134@gmail.com>
2021-09-04 11:53:28 -0600

    Create class messages, create welcome and welcome_not_valid

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,2 +1,14 @@
 class Messages
-end 
+  def initialize
+  end
+
+  def welcome_message
+    puts "Welcome to MASTERMIND
+    Would you like to (p)lay, read the (i)nstructions, or (q)uit?
+    >"
+  end
+
+  def welcome_not_valid
+    puts "That input is not valid.
+    Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
