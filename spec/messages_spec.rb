3e7c1b46521f2e2002e5e59f9ccd8f68ad4676bf
Chris Hewitt <agop5134@gmail.com>
2021-09-04 12:00:32 -0600

    create tests for welcome_not_valid, instructions_message, game_flow

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -18,3 +18,22 @@ RSpec.describe Messages do
    quit_message = ''
    expect(quit_message) = ''
   end
+
+
+  it "tells users their selection is invalid" do
+      messages = Messages.new
+      welcome_not_valid = ''
+      expect(welcome_not_valid).to eq('')
+  end
+
+  it "displays instructions" do
+    messages = Messages.new
+    instructions_message = ''
+    expect(instructions_message).to eq('')
+  end
+
+  it "informs users of the game flow" do
+    messages = Messages.new
+    game_flow_message = ''
+    expect(game_flow_message).to eq('')
+  end
