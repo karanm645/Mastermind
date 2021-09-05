94c383c3a20414f6d6aa20b19bbab8dade3a6e03
Karan Mehta <karanm645@gmail.com>
2021-09-05 13:19:16 -0500

    "Spacing"

--- lib/code_generator.rb
+++ lib/code_generator.rb
@@ -4,14 +4,11 @@ class CodeGenerator
     @colors = ['b','g','r','y']
   end
 
-
   def generate_secret
    4.times.map do |color|
      @colors.sample
    end
-
   end
-
 end
 
  #iterating 4 times thru the string array, using sample method to pull different elements out
