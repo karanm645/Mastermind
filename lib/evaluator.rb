da1da3487273048ed0fba276b073fa6de4604fbb
Karan Mehta <karanm645@gmail.com>
2021-09-05 23:04:35 -0500

    "Create evaluator class add comparison logic"

---
+++ lib/evaluator.rb
@@ -0,0 +1,15 @@
+class Evaluator
+  def initialize
+    @color_code
+    @guess
+  end
+
+  # def color_comparison
+  #   @guess & @color_code
+  #   puts color_comparison.count
+  # end
+  #
+  # def position_comparison
+  #   @color_code.zip(@guess).count {|a,b| a == b}
+  # end
+end
